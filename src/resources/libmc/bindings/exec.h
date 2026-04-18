396e91c9f4f33c62fb094d621ed75332d8fcb149
xia__mc <2052472631@qq.com>
2026-04-18 17:02:58 +0800

    Fix exec string assembly and ignore temp workspace

--- src/resources/libmc/bindings/exec.h
+++ src/resources/libmc/bindings/exec.h
@@ -1,21 +1,31 @@
 #pragma once
 
 #include "stdlib.h"
+#include "stdint.h"
 #include "assert.h"
 
 static inline int exec(const char *cmd) {
+    uintptr_t ptr;
+    unsigned char c;
+
     assert(cmd != NULL);
+    ptr = ((uintptr_t)cmd) << 2;
+    c = *(const unsigned char *)ptr;
     __asm volatile (
         "inline data modify storage std:vm s1 set value %{str: \"\", next: \"\"%}\n"
-        "inline data modify storage std:vm s1.str set from storage std:vm char2str_map[%0]"
+        "inline data modify storage std:vm s1.str set from storage std:vm char2str_map.\"%0\""
         :
-        : "r"(*cmd)
+        : "r"(c)
     );
 
-    char c;
-    while ((c = *++cmd) != NULL) {
+    while ((c = *(const unsigned char *)++ptr) != 0) {
         __asm volatile (
-            "inline data modify storage std:vm s1.next set from storage std:vm char2str_map[%0]"
+            "inline data modify storage std:vm s1.next set from storage std:vm char2str_map.\"%0\"\n"
+            "inline data modify storage std:vm s3 set value %{left: \"\", right: \"\"%}\n"
+            "inline data modify storage std:vm s3.left set from storage std:vm s1.str\n"
+            "inline data modify storage std:vm s3.right set from storage std:vm s1.next\n"
+            "inline function std:_internal/merge_string2 with storage std:vm s3\n"
+            "inline data modify storage std:vm s1.str set from storage std:vm s3.string"
             :
             : "r"(c)
         );
